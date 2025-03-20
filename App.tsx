53355cd41ae4236612938f4935607a1217133cfc
Choquinn <voltzchoquinn@gmail.com>
2025-03-20 08:31:00 -0300

    Criação do projeto

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Esse cachorro vai já morrer viu, só comendo pão com banana</Text>
       <StatusBar style="auto" />
     </View>
   );
